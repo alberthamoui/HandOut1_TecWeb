b284800d31693a0f6f69df7adb4f6d558c0d0754
Albert Hamoui <103454880+alberthamoui@users.noreply.github.com>
2023-08-23 18:53:21 -0300

    handout3 pt.3 feito

--- database.py
+++ database.py
@@ -1,5 +1,22 @@
 import sqlite3
+from dataclasses import dataclass
+
+@dataclass
+class Note:
+    id: int = None
+    title: str = None
+    content: str = ''
+
+
 class Database:
     def __init__(self, DB_FILENAME) -> None:
         self.conn = sqlite3.connect('{}.db'.format(DB_FILENAME))
         self.conn.execute('CREATE TABLE IF NOT EXISTS note(id INTEGER PRIMARY KEY, title TEXT, content TEXT NOT NULL)')
+        self.conn.commit()
+
+
+    def add(self, note):
+        sql = "INSERT INTO note (title, content) VALUES (?, ?)"
+        values = (note.title, note.content)
+        self.conn.execute(sql, values)
+        self.conn.commit()
