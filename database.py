5a2ac2b817de1d9b73e9b708ecb4c8e440e8e055
Albert Hamoui <103454880+alberthamoui@users.noreply.github.com>
2023-08-25 13:03:08 -0300

    H3 P5 E6

--- database.py
+++ database.py
@@ -14,7 +14,6 @@ class Database:
         self.conn.execute('CREATE TABLE IF NOT EXISTS note(id INTEGER PRIMARY KEY, title TEXT, content TEXT NOT NULL)')
         self.conn.commit()
 
-
     def add(self, note):
         sql = "INSERT INTO note (title, content) VALUES (?, ?)"
         values = (note.title, note.content)
@@ -36,5 +35,11 @@ class Database:
     def update(self, note):
         sql = "UPDATE note SET title = ?, content = ? WHERE id = ?"
         values = (note.title, note.content, note.id)
+        self.conn.execute(sql, values)
+        self.conn.commit()
+
+    def delete(self, note_id):
+        sql = "DELETE FROM note WHERE id = ?"
+        values = (note_id,)
         self.conn.execute(sql, values)
         self.conn.commit()
